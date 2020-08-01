04699ce978e22af663f9b535a02b18f81a7283a5
Catherine <wambukukiiru@gmail.com>
2020-08-02 01:43:43 +0300

    added hover effect on the portfolio section

--- index.js
+++ index.js
@@ -2,4 +2,10 @@ $(document).ready(function(){
     $("img").click(function(){
         $(".clickable").toggle();  
     });
+    $("porfolio").hover(function(){
+        $("portfolio").css("background-color", "yellow")
+        }, function(){
+            $("portfolio").css("background-color","pink")
+
+});
 });
