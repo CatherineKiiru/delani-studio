44a290479a2c04a4d8baa0523778726218b2a378
Catherine <wambukukiiru@gmail.com>
2020-08-02 12:20:04 +0300

    added flex wrap styling to portfolio

--- index.js
+++ index.js
@@ -2,10 +2,5 @@ $(document).ready(function(){
     $("img").click(function(){
         $(".clickable").toggle();  
     });
-    $("porfolio").hover(function(){
-        $("portfolio").css("background-color", "yellow")
-        }, function(){
-            $("portfolio").css("background-color","pink")
+});
 
-});
-});
